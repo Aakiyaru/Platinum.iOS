7c84b70cc09eaea98b681826eaa7fc98ee1eeb8b
Aakiyaru <JellyBall@mail.ru>
2023-04-24 15:03:20 +0700

    first commit

---
+++ Platinum.iOS/Platinum_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Platinum_iOSApp.swift
+//  Platinum.iOS
+//
+//  Created by User on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Platinum_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
